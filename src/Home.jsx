daf5b46751ed07c8feffe64ed841abfcf254bcb9
Marwamu <marwamohamed1293@gmail.com>
2024-08-29 23:21:22 +0200

    lesson #17

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,24 +7,29 @@ const Home = () => {
   const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
-    
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-       setBlogs(data);
-    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
-     {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
