d622f317e80032e9f33bf3f13262f39c9d1c1124
Marwamu <marwamohamed1293@gmail.com>
2024-09-04 16:54:25 +0300

    Lesson #20

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,12 @@
-import { useState, useEffect } from "react";
+
 import "./index.css";
 import BlogList from "./BlogLIst";
+import UseFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+const {data: blogs, isPending, error} = UseFetch("http://localhost:8000/blogs")
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data from that resources");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-
-        });
-    }, 1000);
-  }, []);
 
   return (
     <div className="home">
