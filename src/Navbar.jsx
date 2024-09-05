c0bb9af96f8ab2ee13933251406cab8f0da68831
Marwamu <marwamohamed1293@gmail.com>
2024-09-05 15:07:06 +0300

    Lesson #23

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
+
+
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={{
             color: "white",
             borderRadius: "8px",
@@ -13,7 +17,7 @@ const Navbar = () => {
           }}
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
